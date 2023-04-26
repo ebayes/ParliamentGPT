6d90913cb1dcde5b72fba03fa575c301f07242b8
Ed Bayes <51138168+ebayes@users.noreply.github.com>
2023-04-26 14:03:02 -0400

    Update 1_🖋️_AutoScribe.py

--- pages/1_🖋️_AutoScribe.py
+++ pages/1_🖋️_AutoScribe.py
@@ -270,7 +270,7 @@ with tab1:
 
         tone = st.multiselect('Select a tone:', ['Formal', 'Sympathetic', 'Informal', 'Pirate'], ['Formal'])
 
-        word_count = st.slider("Choose a word count:", min_value = 200, max_value = 400, value = 300)
+        word_count = st.slider("Choose a word count:", min_value = 200, max_value = 400, value = 250)
 
         doc_ID = st.text_input(label="Google Docs ID:", value = "https://docs.google.com/document/d/1qhd6sbq5aTeBz3WGATrgyEZ1X456mWsz3ZsJywngx4Q/edit") 
         if doc_ID is not None:
