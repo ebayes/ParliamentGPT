2ef38dcb46d2fef92baea2dcccfd84812297e15e
Ed Bayes <51138168+ebayes@users.noreply.github.com>
2023-04-26 00:56:03 -0400

    Update and rename Hello.py to 👋_Home_.py

---
+++ 👋_Home_.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(layout="centered", page_icon="🏛", page_title="ParliamentGPT")
+st.write("# Welcome to ParliamentGPT! 👋")
+st.markdown(
+        """
+        - 🏛 **ParliamentGPT is a suite of tools to help policymakers become more productive.** 
+        - 🤝 **Built by policymakers for policymakers.** ParliamentGPT helps you: write letters to constituents in a few clicks, summarize reports in seconds, or even ask questions about legislation. ParliamentGPT frees up time so you can spend more time helping your constituents!
+        - 👈 **Select a demo from the left** to see some examples of what ParliamentGPT can do!
+
+        ### Current demos
+
+        - 🖋️ **AutoScribe** – Policymakers respond to thousands of letters a year. AutoScribe drafts responses based on your FAQs and even downloads them in your own template, all in a few clicks! 
+        - **Coming soon:** read letters from an image, even handwritten ones!
+        - 🧑‍⚖ **eLibrarian** – Policymakers review hundreds of reports and pieces of legislation a year. eLibrarian is a chatbot that trains a custom AI model on legislation or reports that you upload, so you can ask questions about it in real time. No more ctrl-F!
+
+        ### Sign up for more features
+
+        - 💡 **Want to access more features or demos, or have an idea for how we can improve?** Sign up to create a free account [here](www.parliamentgpt.co.uk/signup).
+        
+    """
+    )
+
